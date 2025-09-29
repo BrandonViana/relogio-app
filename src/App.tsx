bcd3422a2e4a8e35e0aabeed0d32d4379f7e05ed
Brandon Viana <brandon.rodrigues.101@gmail.com>
2025-09-29 11:53:38 -0300

    changed the main color

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import DigitalClock from "./components/DigitalClock";
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-100 dark:bg-gray-950 flex flex-col transition-colors duration-300">
+    <div className="min-h-screen bg-white dark:bg-gray-950 flex flex-col transition-colors duration-300">
       <div className="text-4xl p-6">
         <DarkLight />
       </div>
       <div className="text-black font-bold text-6xl p-3 justify-center text-center dark:text-white transition-colors duration-300">
         <header className="justify-between">
-          <span>Relógio</span>
+          <span>Relógio Digital</span>
         </header>
       </div>
       <div className="flex-grow flex justify-center items-center text-black dark:text-white transition-colors duration-300">
