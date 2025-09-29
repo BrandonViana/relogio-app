1022a4b3bc1f6b6e216c42b95944be242312f557
Brandon Viana <brandon.rodrigues.101@gmail.com>
2025-09-29 08:11:02 -0300

    deleted hero.ts

--- hero.ts
+++
@@ -1,6 +0,0 @@
-// hero.ts
-import { heroui } from "@heroui/react";
-export default heroui();
-@plugin './hero.ts';
-@source '../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}';
-@custom-variant dark (&:is(.dark *));
