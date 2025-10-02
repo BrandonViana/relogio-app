8fbb9f77aa28058fd47b13b4067b5f6af2cd83df
Brandon Viana <brandon.rodrigues.101@gmail.com>
2025-10-02 11:49:26 -0300

    removed the react.svg file, and optimized the app file adding a new file to the head and body

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import DarkLight from "./components/DarkLight";
-import DigitalClock from "./components/DigitalClock";
+import HeaderBody from "./components/header_body/HeaderBody";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-950 flex flex-col transition-colors duration-300">
-      <div className="text-4xl p-6">
-        <DarkLight />
-      </div>
-      <div className="text-black font-bold text-6xl p-3 justify-center text-center dark:text-white transition-colors duration-300">
-        <header className="justify-between">
-          <span>Relógio Digital</span>
-        </header>
-      </div>
-      <div className="flex-grow flex justify-center items-center text-black dark:text-white transition-colors duration-300">
-        <DigitalClock />
-      </div>
+    <div>
+      <HeaderBody />
     </div>
   );
 }
---
+++ src/components/header_body/HeaderBody.tsx
@@ -0,0 +1,22 @@
+import DigitalClock from "../clock/DigitalClock";
+import DarkLight from "../switch_light/DarkLight";
+
+const HeaderBody = () => {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-950 flex flex-col transition-colors duration-300">
+      <div className="text-4xl p-6">
+        <DarkLight />
+      </div>
+      <div className="text-black font-bold text-6xl p-3 justify-center text-center dark:text-white transition-colors duration-300">
+        <header className="justify-between">
+          <span>Relógio Digital</span>
+        </header>
+      </div>
+      <div className="flex-grow flex justify-center items-center text-black dark:text-white transition-colors duration-300">
+        <DigitalClock />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBody;
